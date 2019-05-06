e9e016cc097fe83b4358942e24b22f2b511cd5f3
tsaihuangsd <tsaihuangsd@gmail.com>
2019-05-06 16:32:24 -0700

    Implemented task card component

--- src/components/GroupTasks.js
+++ src/components/GroupTasks.js
@@ -1,19 +1,55 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import "./Styles/GroupTask.css";
+import TaskCard from "./TaskCard";
+import {
+    MDBCard,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBCardText,
+    MDBBtn,
+    MDBModal,
+    MDBModalBody,
+    MDBModalHeader,
+    MDBModalFooter,
+    MDBInput
+  } from "mdbreact";
 
 class GroupTasks extends Component {
-    constructor() {
-        super();
-        this.state= {
+    // constructor(props) {
+    //     super();
+        state= {
+            tasks:[],
+            searchField: "",
 
-        };
-    }
+
+        // };
+        }
 
 render() {
     return (
         <div className="task-container">
-            <h1>Task Trak!!!</h1>
+            <div className="tasks-cards">
+                {/* {this.props.groupTasks !== null
+                    ? this.props.userGroups.map(group => ( */}
+                        <TaskCard
+                            task={1}
+                            taskname={"Walk Dog"}
+                            requestedBy={"Alex"}
+                            done={0}
+                            comments={0}
+                            repeated={0}
+                            assignee={"Tsai"}
+                            done={false}
+                            // group={1}
+                            // updateGroup={this.saveGroupName}
+                            // removeGroup={this.deleteGroup}
+                        />
+                    {/* ))
+                    : null
+                } */}
+
+            </div>
             <Link to={`/groups/${this.props.match.params.id}`}><button> Shop Trak!!!</button></Link>
         </div>
     )
---
+++ src/components/TaskCard.js
@@ -0,0 +1,79 @@
+import React from "react";
+import commentImg from '../images/comment-img.jpg';
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardHeader,
+  MDBNavLink,
+  MDBCardFooter,
+  MDBIcon,
+  MDBCardTitle,
+  MDBBtn,
+  MDBInput
+} from "mdbreact";
+import { withRouter } from "react-router-dom";
+import "./Styles/TaskCard.css";
+
+const TaskCard = props => {
+  return (
+      <div className="task-card">
+        <div className="task-card-left">
+             <h7>{props.task}</h7>
+             <h7>Requested by: {props.requestedBy}</h7>
+        </div>
+        <div className="task-card-middle">
+            <h5>{props.assignee}</h5>
+        </div>
+        <div className="task-card-right">
+            <img src={commentImg} height="30" width="30"></img>
+            <input type="checkbox" name="vehicle" value="Bike"></input>
+            <h7>Done</h7>
+        </div>
+      </div>
+    // <MDBCard className="task-card">
+    //     <MDBCardTitle>{props.task}</MDBCardTitle>
+    //     <
+        
+    // </MDBCard>
+
+    // <div className="task-card">
+    //   <MDBCard className="text-center">
+    //     <MDBCardHeader color="primary-color" tag="h3">
+    //       {props.task.name}
+    //     </MDBCardHeader>
+    //     <MDBCardBody>
+    //       <MDBNavLink key={props.key} to={`/groups/${props.group.id}`}>
+    //         <MDBBtn color="success">Enter</MDBBtn>
+    //       </MDBNavLink>
+    //     </MDBCardBody>
+    //     {
+    //       props.group.userID !== Number(localStorage.getItem("userId")) ? null :
+    //           <MDBCardFooter style={{ background: "#2A922D" }}>
+    //             <div className="group-card-footer">
+    //               <div
+    //                   className="group-card-footer-button"
+    //                   onClick={() =>
+    //                       props.updateGroup(props.group.id, props.group.name)
+    //                   }
+    //               >
+    //                 <MDBIcon icon="edit" />
+    //               </div>
+
+    //               <div
+    //                   className="group-card-footer-button"
+    //                   onClick={() =>
+    //                       props.removeGroup(props.group.id, props.group.name)
+    //                   }
+    //               >
+    //                 <MDBIcon icon="trash" />
+    //               </div>
+    //             </div>
+    //           </MDBCardFooter>
+    //     }
+
+    //   </MDBCard>
+    // </div>
+  );
+};
+
+export default withRouter(TaskCard);
