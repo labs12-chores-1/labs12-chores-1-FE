83e80a0dd2f7175404507e03c0269b8a417b7fa8
ItelSunday <iteldomingotech@gmail.com>
2019-05-06 10:46:46 -0700

    created GroupTaskView

---
+++ src/components/GroupTaskView.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class GroupTasks extends Component {
+    constructor() {
+        super();
+        this.state= {
+
+        };
+    }
+
+render() {
+    return (
+        <div className="taskView-container">
+            task app
+        </div>
+    )
+    }
+}
+
+export default GroupTasks;
