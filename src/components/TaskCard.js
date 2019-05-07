fadfcd247d169711794e6ec83110f5d7173b2983
Estevan <estevan.rodriguez10@yahoo.com>
2019-05-07 13:20:06 -0700

    finished redux sequence for get comments

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from 'react-redux';
 import commentImg from '../images/comment-img.jpg';
 import {
   MDBCard,
@@ -7,7 +8,15 @@ import {
 import { withRouter } from "react-router-dom";
 import "./Styles/TaskCard.css";
 
+import { getTaskComments } from '../store/actions/rootActions';
+
 const TaskCard = props => {
+  
+  const getComments = e => {
+    e.preventDefault();
+    props.getTaskComments(props.match.params.id);
+  };
+
   return (
       <MDBCard className="task-card" 
         onClick={()=>props.history.push(`/task/${props.taskID}`)}
@@ -21,7 +30,7 @@ const TaskCard = props => {
                 <h5>{props.assignee}</h5>
             </div>
             <div className="task-card-right">
-                <img src={commentImg} height="30" width="30"></img>
+                <img onClick ={getComments} src={commentImg} height="30" width="30"></img>
                 <input type="checkbox" name="vehicle" value="Bike"></input>
                 <h7>Done</h7>
             </div>
@@ -30,5 +39,13 @@ const TaskCard = props => {
     
   );
 };
+const mapStateToProps = state => {
+  state = state.rootReducer; // pull values from state root reducer
+  return {
+    //state items
+    taskComments: state.taskComments,
+    errorMessage: state.errorMessage
+  };
+};
 
-export default withRouter(TaskCard);
+export default withRouter(connect(mapStateToProps,{getTaskComments})(TaskCard));
